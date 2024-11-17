b46f7429dd4a49786c6d99d1e55cebb7ef54b7aa
Jeswin Thampichan Joseph <140978348+jeswin1811@users.noreply.github.com>
2024-11-17 17:33:18 +0530

    Update MergeSort.c

--- MergeSort.c
+++ MergeSort.c
@@ -9,74 +9,60 @@
  *    Description: This is a C program to sort an array using the merge sort     
  *                 algorithm.
  ***********************************************************************************/
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 
-void merge(int arr[], int l, int m, int r){
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int L[n1], R[n2];
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        }
-        else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+void merge(int a[], int low, int mid, int high){
+	int left = low;
+	int right = mid+1;
+	int i = low;
+	int temp[100];
+	while(left<=mid && right<=high){
+		if(a[left] <= a[right]){
+			temp[i++] = a[left++];
+		}
+		else{
+			temp[i++] = a[right++];
+		}
+	}
+	while(left <= mid){
+		temp[i++] = a[left++];
+	}
+	while(right <= high){
+		temp[i++] = a[right++];
+	}
+	for(int j=low; j<=high; j++){
+		a[j] = temp[j];
+	}
 }
 
-void mergeSort(int arr[], int l, int r){
-    if (l < r) {
-        int m = l + (r - l) / 2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-
-        merge(arr, l, m, r);
-    }
+void mergesort(int a[], int low, int high){
+	if(low < high){
+		int mid = (low+high)/2;
+		mergesort(a,low,mid);
+		mergesort(a,mid+1,high);
+		merge(a,low,mid,high);
+	}
 }
 
-void printArray(int A[], int size){
-    int i;
-    for (i = 0; i < size; i++)
-        printf("%d ", A[i]);
-    printf("\n");
+void main(){
+	int n;
+	printf("\nEnter the number of elements: ");
+	scanf("%d",&n);
+	int a[n];
+	printf("\nEnter the elements of the array:\n");
+	for(int i=0; i<n; i++){
+		scanf("%d",&a[i]);
+	}
+	printf("\nThe unsorted array is:\n");
+	for(int i=0; i<n; i++){
+		printf("%d\t",a[i]);
+	}
+	mergesort(a,0,n-1);
+	printf("\nThe sorted array is:\n");
+	for(int i=0; i<n; i++){
+	printf("%d\t",a[i]);
+	}
+	printf("\n");
 }
 
-int main(){
-    int arr_size;
-    printf("Enter the number of elements: ");
-    scanf("%d", &arr_size);
-    int arr[arr_size];
-    printf("Enter %d elements:\n", arr_size);
-    for (int i = 0; i < arr_size; i++) {
-        scanf("%d", &arr[i]);
-    }
-    printf("Given array is:\n");
-    printArray(arr, arr_size);
-    mergeSort(arr, 0, arr_size - 1);
-    printf("\nSorted array is:\n");
-    printArray(arr, arr_size);
-    return 0;
-}
+
